9806fc125f3a6f50d8566ad633b912d237ac79b0
Lenovo <Lenovo@DESKTOP-DSMFSPI>
2024-07-23 11:30:37 +0200

    Ajoutez des fichiers projet.

---
+++ CourceMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourceMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
